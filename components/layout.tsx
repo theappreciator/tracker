975123d095f2af4bb135cdd67ed33428e16aaf45
Jess Tucker <jessjesstheappreciator@gmail.com>
2023-08-27 14:31:24 -0400

    Now supports PWA

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,8 +11,9 @@ import { title } from 'process'
 import { useState } from 'react'
 import SettingsDialog from '../src/components/Dialogs/SettingsDialog'
 
-const name = 'Thing Tracker'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = "Thing Tracker";
+export const siteDescription = "Track your things";
+export const siteDomain = "https://thingtracker.fly.dev";
 
 export default function Layout({
   children,
@@ -61,7 +62,7 @@ export default function Layout({
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="Let's track some things"
+            content={siteDescription}
           />
           <meta
             property="og:image"
@@ -71,6 +72,57 @@ export default function Layout({
           />
           <meta name="og:title" content={siteTitle} />
           <meta name="twitter:card" content="summary_large_image" />
+
+
+          <meta name="application-name" content={siteTitle} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content={siteTitle} />
+          <meta name="description" content={siteDescription} />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#460368" />
+
+          <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
+          <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
+
+          <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content={siteDomain} />
+          <meta name="twitter:title" content={siteTitle} />
+          <meta name="twitter:description" content={siteDescription} />
+          <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
+          <meta name="twitter:creator" content="@DavidWShadow" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content={siteTitle} />
+          <meta property="og:description" content={siteDescription} />
+          <meta property="og:site_name" content={siteTitle} />
+          <meta property="og:url" content={siteDomain} />
+          <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" />
+
+          <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
+          <link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
+          <link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
+          <link rel='apple-touch-startup-image' href='/images/apple_splash_1125.png' sizes='1125x2436' />
+          <link rel='apple-touch-startup-image' href='/images/apple_splash_1242.png' sizes='1242x2208' />
+          <link rel='apple-touch-startup-image' href='/images/apple_splash_750.png' sizes='750x1334' />
+          <link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' />
+
+          <meta
+            name='viewport'
+            content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+          />
+
         </Head>
         <header className={styles.header}>
           <Container component="main" maxWidth="lg">
@@ -82,7 +134,7 @@ export default function Layout({
               }}
             >
               <Typography component="h1" variant="h4">
-                {name}
+                {siteTitle}
                 {loggedIn && (
                   <IconButton aria-label="delete" size="large" onClick={handleSettingsClick}>
                     <SettingsIcon fontSize="inherit" />
