d48c0939977f38d93d63b279cf706c8c0e5649c6
Jess Tucker <jessjesstheappreciator@gmail.com>
2023-09-07 08:57:15 -0400

    Fix unique key for list rows

--- pages/u/[email]/thing/[id].tsx
+++ pages/u/[email]/thing/[id].tsx
@@ -14,6 +14,7 @@ import { convertDateStringYyyyMmDdToFullNoYear, getTimeStringCorrectedForTimezon
 import AddIcon from '@mui/icons-material/Add';
 import YesNoIcon from '../../../../src/components/Icons/YesNoIcon';
 import { generateSkeletonDateGroupThings } from '../../../../util/generators/dateGroupThing';
+import React from 'react';
 
 
 const getThingsAndActions = async (thingId: number): Promise<IDateThingGroup[]> => {
@@ -55,48 +56,48 @@ export default function ThingDetailPage(
         <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
           <List sx={{ marginTop: "1rem" }}>
             {itemsToDisplay.map((d, i) => {
+              const listKey = `list-date-${d.date}`
               const isCountable = d.groups.find(g => true)?.things.find(t => true)?.actions.find(a => true)?.type === ActionType.count;
               const totalForDate = d.groups.map(g => g.things.map(t => typeof t.count === "number" ? t.count : 0).reduce((prev, cur) => prev + cur, 0)).reduce((prev, cur) => prev + cur, 0);
               const goalForDate = d.groups.find(g => true)?.things.find(t => true)?.goal;
               const isSuccess = goalForDate ? totalForDate >= goalForDate: false;
               return (
-                <>
-                  <nav aria-label={`list for ${d.date}`}>
-                    {showSkeleton && <Skeleton animation="pulse" width={"15rem"} height={"2.75rem"}/>}
-                    {!showSkeleton && (
-                      <ListSubheader sx={{ backgroundColor: theme.palette.grey[200], borderRadius: "0.5rem", paddingTop: "0.4rem", paddingBottom: "0.4rem" }}>
-                        <Box sx={{ display: "flex", alignItems: "center", justifyContent: "flex-start" }}>
-                            <Typography sx={{ flexGrow: 1, fontWeight: 700, color:theme.palette.grey[900] }}>
-                              {convertDateStringYyyyMmDdToFullNoYear(d.date)}
-                            </Typography>
-                          {!!(isCountable && goalForDate) && <YesNoIcon actionValue={isSuccess ? 1 : -1} placement="button" />}
-                          {isCountable && (
-                            <Typography sx={{ fontWeight: 700, color:theme.palette.grey[900] }}>
-                              {totalForDate}{goalForDate ? `/${goalForDate}` : ''}
-                            </Typography>
-                          )}
-                        </Box>
-                      </ListSubheader>
-                    )}
-                    {d.groups.map(g => g.things.map(t => (
-                      <>
+                <nav key={listKey} aria-label={`list for ${d.date}`}>
+                  {showSkeleton && <Skeleton key={listKey+"skeleton"} animation="pulse" width={"15rem"} height={"2.75rem"}/>}
+                  {!showSkeleton && (
+                    <ListSubheader  sx={{ backgroundColor: theme.palette.grey[200], borderRadius: "0.5rem", paddingTop: "0.4rem", paddingBottom: "0.4rem" }}>
+                      <Box sx={{ display: "flex", alignItems: "center", justifyContent: "flex-start" }}>
+                          <Typography sx={{ flexGrow: 1, fontWeight: 700, color:theme.palette.grey[900] }}>
+                            {convertDateStringYyyyMmDdToFullNoYear(d.date)}
+                          </Typography>
+                        {!!(isCountable && goalForDate) && <YesNoIcon actionValue={isSuccess ? 1 : -1} placement="button" />}
+                        {isCountable && (
+                          <Typography sx={{ fontWeight: 700, color:theme.palette.grey[900] }}>
+                            {totalForDate}{goalForDate ? `/${goalForDate}` : ''}
+                          </Typography>
+                        )}
+                      </Box>
+                    </ListSubheader>
+                  )}
+                  {d.groups.map(g => g.things.map((t, i) => {
+                    const rowKey = `row-${d.date}-${t.thingId}-${i}`
+                    return (
+                      <React.Fragment key={rowKey}>
                         {showSkeleton && <Skeleton animation="pulse" width="10rem" height="2rem"/>}
                         {!showSkeleton && (
-                          <ListItem disablePadding>
-                            <ListItem sx={{ display: "flex", alignItems: "center", justifyContent: "flex-start" }}>
-                              <Typography sx={{ flexGrow: 1, color:theme.palette.grey[900] }}>
-                                {`${t.dateTime ? getTimeStringCorrectedForTimezone(t.dateTime, DEFAULT_USER_LOCALE, DEFAULT_USER_TIMEZONE) : 'n/a'}`}
-                              </Typography>
-                              <Typography>
-                                {t.dateTime ? t.count : ''}
-                              </Typography>
-                            </ListItem>
+                          <ListItem sx={{ display: "flex", alignItems: "center", justifyContent: "flex-start" }}>
+                            <Typography sx={{ flexGrow: 1, color:theme.palette.grey[900] }}>
+                              {`${t.dateTime ? getTimeStringCorrectedForTimezone(t.dateTime, DEFAULT_USER_LOCALE, DEFAULT_USER_TIMEZONE) : 'n/a'}`}
+                            </Typography>
+                            <Typography>
+                              {t.dateTime ? t.count : ''}
+                            </Typography>
                           </ListItem>
                         )}
-                      </>
-                    )))}
-                  </nav>
-                </>
+                      </React.Fragment>
+                    )
+                  }))}
+                </nav>
               );
             })}
           </List>
